92109a9ae819a42056585d33799b81583d75e5a3
503_17 <503_17@DESKTOP-PAJ97JU>
2019-03-12 14:08:00 +0900

    2/11 First JAVA

---
+++ project0211/src/project0211/First.java
@@ -0,0 +1,13 @@
+package project0211;
+
+public class First {    //클래스 선언
+
+	public static void main(String[] args) {    
+		//메인메소드 선언  _ 메소드 옆에는 항상 ()가 있어야된대.
+		// TODO Auto-generated method stub
+			System.out.println("첫번째 자바 프로그램");
+			System.out.println("첫번째 자바 프로그램");
+	}
+}
+//system.out.print는 전부 모아서 한번에 출력. 한번 출력하고 난 후 바로 뒤에 이어서 출력
+//           println은 한줄씩 차례로 출력.
---
+++ project0211/src/project0211/Second.java
@@ -0,0 +1,22 @@
+package project0211;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("안녕"+"하세요.\n오늘은\t" +1+"일차 수업입니다.");
+			// \n은 줄바꿈 '; \t는 탭
+			System.out.println("3 + 4 ="+ 3+4);
+			System.out.println("3 + 4 ="+(3+4));
+
+			//정수출력
+			System.out.println(10);			// 10진수 10
+			System.out.println(0x10);			// 16진수 10
+			System.out.println(010);			// 8진수 10
+			// 실수출력
+			System.out.println(3.14f);	//float 실수
+			System.out.println(3.14);		//double 실수
+			System.out.println(0.314E1);	//double실수(지수표현)
+	}
+
+}
