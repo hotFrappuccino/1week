8f7affea44a0208a1aaf4eb0dc82795cd85299c7
503_17 <503_17@DESKTOP-PAJ97JU>
2019-03-12 14:09:38 +0900

    0211_2

---
+++ projec0211_2/src/projec0211_2/Second.java
@@ -0,0 +1,14 @@
+package projec0211_2;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("안녕"+"하세요.");
+			System.out.println("오늘은 "+1+"일차 수업입니다.");
+			System.out.println("3 + 4 ="+ 3+4);
+			System.out.println("3 + 4 ="+(3+4));
+			System.out.println("3*4 ="+ 3*4);
+	}
+
+}
